f8838f33674699e150d708f25ed0780a6c0e2725
Zied310 <ziedzhiri@yahoo.fr>
2024-10-21 16:50:36 +0100

    added login screen

--- app/src/main/java/com/example/nascar_app/MainActivity.kt
+++ app/src/main/java/com/example/nascar_app/MainActivity.kt
@@ -3,15 +3,23 @@ package com.example.nascar_app
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Lock
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import com.example.nascar_app.screens.NewsScreen
 import com.example.nascar_app.ui.theme.Nascar_appTheme
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -21,8 +29,64 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    mainAppScreen(modifier = Modifier.fillMaxSize())
+                    loginScreen(modifier = Modifier.fillMaxSize())
                 }
+            }
+        }
+    }
+
+
+    @ExperimentalMaterial3Api
+    @Composable
+    fun loginScreen(modifier: Modifier) {
+        var email by remember { mutableStateOf("") }
+        var password by remember { mutableStateOf("") }
+        Column(
+            modifier = modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Top
+
+        ) {
+
+            Image(
+                painter = painterResource(id = R.drawable.ic_logo),
+                contentDescription = "Nascar Logo",
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(200.dp)
+            )
+
+            Spacer(modifier = Modifier.height(164.dp))
+
+            OutlinedTextField(
+                value = email,
+                onValueChange = { email = it },
+                label = { Text("Email") },
+                trailingIcon = {
+                    IconButton(
+                        onClick = { /*TODO*/ }) {
+                        Icon(Icons.Filled.Email, contentDescription = "Email")
+                    }
+                }
+
+            )
+            Spacer(modifier = Modifier.height(12.dp))
+            OutlinedTextField(
+                value = password,
+                onValueChange = { password = it },
+                label = { Text("Password") },
+                trailingIcon = {
+                    IconButton(
+                        onClick = { /*TODO*/ }) {
+                        Icon(Icons.Filled.Lock, contentDescription = "Email")
+                    }
+                }
+            )
+
+            Spacer(modifier = Modifier.height(12.dp))
+
+            Button(onClick = { /*TODO*/ }) {
+                Text("Login")
             }
         }
     }
